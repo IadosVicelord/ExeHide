cb59f84b6d6c7f9a5d972264b368b72668452247
IadosVicelord <IadosVicelord@github.com>
2020-01-20 12:42:07 +0400

    comments

--- ExeHide/Models/GeneralModel.cs
+++ ExeHide/Models/GeneralModel.cs
@@ -11,69 +11,81 @@ namespace ExeHide.Models
 {
     static class GeneralModel
     {
+        /// <summary>
+        /// Генерация исполняемого файла
+        /// </summary>
+        /// <param name="filepathHidden">Путь к файлу</param>
+        /// <param name="launcherOutPath">Путь, куда нужно разместить сгенерированный файл</param>
+        /// <param name="password">Пароль</param>
         public static void GenerateExecutableSharpCode(string filepathHidden, string launcherOutPath, string password)
         {
+            //Изменение формата пути к файлу.
             string[] temp = filepathHidden.Split('\u005c');
             filepathHidden = String.Join("/", temp);
 
+            //Объект компитялтора
             CodeCompileUnit Compiler = new CodeCompileUnit();
             CompilerParameters Cparams = new CompilerParameters();
 
+            //Определение нового пространства имен и импортов
             CodeNamespace ProtectedLauncher = new CodeNamespace("ProtectedLauncher");
             ProtectedLauncher.Imports.Add(new CodeNamespaceImport("System"));
             ProtectedLauncher.Imports.Add(new CodeNamespaceImport("System.IO"));
             ProtectedLauncher.Imports.Add(new CodeNamespaceImport("System.Diagnostics"));
 
+            //Добавление пространства имен в компилятор
             Compiler.Namespaces.Add(ProtectedLauncher);
 
+            //Определение основного класса
             CodeTypeDeclaration Program = new CodeTypeDeclaration();
             Program.Name = "Program";
             Program.IsClass = true;
             Program.Attributes = MemberAttributes.Public;
             ProtectedLauncher.Types.Add(Program);
 
+            //Определение точки входа
             CodeEntryPointMethod Main = new CodeEntryPointMethod();
             Main.Name = "Main";
             Main.Attributes = MemberAttributes.Public | MemberAttributes.Static;
 
+            //Определение исходного кода генерируемого файла
             #region sourceCode
             CodeSnippetExpression expPassword = new CodeSnippetExpression($"string password = \"{password}\";");
             CodeSnippetExpression expReadPassword = new CodeSnippetExpression("string input = Console.ReadLine();");
             CodeSnippetExpression expIf = new CodeSnippetExpression("if(input == password) {");
-            CodeSnippetExpression exp1 = new CodeSnippetExpression("System.Diagnostics.Process process = new System.Diagnostics.Process();");
-            CodeSnippetExpression exp2 = new CodeSnippetExpression($"process.StartInfo.FileName = \"{filepathHidden}\";");
-            CodeSnippetExpression exp3 = new CodeSnippetExpression("process.Start(); }");
+            CodeSnippetExpression newProcess = new CodeSnippetExpression("System.Diagnostics.Process process = new System.Diagnostics.Process();");
+            CodeSnippetExpression setStartInfo = new CodeSnippetExpression($"process.StartInfo.FileName = \"{filepathHidden}\";");
+            CodeSnippetExpression startProcess = new CodeSnippetExpression("process.Start(); }");
 
             CodeExpressionStatement cesPassword = new CodeExpressionStatement(expPassword);
             CodeExpressionStatement cesRead = new CodeExpressionStatement(expReadPassword);
             CodeExpressionStatement cesIf = new CodeExpressionStatement(expIf);
-            CodeExpressionStatement ces1 = new CodeExpressionStatement(exp1);
-            CodeExpressionStatement ces2 = new CodeExpressionStatement(exp2);
-            CodeExpressionStatement ces3 = new CodeExpressionStatement(exp3);
+            CodeExpressionStatement cesNewProcess = new CodeExpressionStatement(newProcess);
+            CodeExpressionStatement cesSetStartInfo = new CodeExpressionStatement(setStartInfo);
+            CodeExpressionStatement cesStartProcess = new CodeExpressionStatement(startProcess);
 
+            //Добавление строк кода в функцию Main
             Main.Statements.Add(cesPassword);
             Main.Statements.Add(cesRead);
             Main.Statements.Add(cesIf);
-            Main.Statements.Add(ces1);
-            Main.Statements.Add(ces2);
-            Main.Statements.Add(ces3);
+            Main.Statements.Add(cesNewProcess);
+            Main.Statements.Add(cesSetStartInfo);
+            Main.Statements.Add(cesStartProcess);
 
+            //Добавление функции-точки входа в качестве члена класса
             Program.Members.Add(Main);
             #endregion
 
+            //Установка параметров компилятора
             Cparams.ReferencedAssemblies.Add("System.dll"); // System, System.Net, etc namespaces
-
             Cparams.GenerateInMemory = false;
             Cparams.GenerateExecutable = true;
-
             Cparams.MainClass = "ProtectedLauncher.Program";
-
             Cparams.OutputAssembly = launcherOutPath;
-
             CSharpCodeProvider provider = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v3.5" } });
 
+            //Запись результатов компиляции и их вывод
             CompilerResults results = provider.CompileAssemblyFromDom(Cparams, Compiler);
-            //MessageBox.Show(results.TempFiles.BasePath);
             if (results == null || results.Errors.Count > 0)
             {
                 foreach (var a in results.Errors)
@@ -82,10 +94,16 @@ namespace ExeHide.Models
             else MessageBox.Show("Done!", "", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
+        /// <summary>
+        /// Заменяет оригинальный исполняемый файл на генерируемое консольное приложение с паролем
+        /// </summary>
+        /// <param name="password">Устанавливаемый пароль</param>
         public static void SetPassword(string password)
         {
             string ExeFile = "";
             string HiddenExeFile = "";
+
+            //Выбор исполняемого файла
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Text files(*.exe) | *.exe";
             if (openFileDialog.ShowDialog() == true)
@@ -95,23 +113,31 @@ namespace ExeHide.Models
 
             if (ExeFile.Contains(".exe"))
             {
+                //Замена названия оригинального файла
                 HiddenExeFile = ExeFile.Replace(".exe", "") + "hidd.exe";
                 File.Move(ExeFile, HiddenExeFile);
+                //Сокрытие оригинального файла
                 File.SetAttributes(HiddenExeFile, FileAttributes.Hidden);
+                //Генерация нового исполняемого файла
                 GenerateExecutableSharpCode(HiddenExeFile, ExeFile, password);
             }
         }
 
+        /// <summary>
+        /// Возвращает оригинальный файл на прежнее место и удаляет подмененный файл
+        /// </summary>
         public static void UnsetPassword()
         {
             string ExeFile = "";
             string HiddenExeFile = "";
+            //Выбор исполняемого файла
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Text files(*.exe) | *.exe";
             if (openFileDialog.ShowDialog() == true)
             {
                 ExeFile = openFileDialog.FileName;
             }
+            //Возвращение оригинального файла на место
             HiddenExeFile = ExeFile.Replace(".exe", "hidd.exe");
             if (File.Exists(HiddenExeFile))
             {
